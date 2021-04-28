6eb45d054dc0301f431dfe4a831bb1e886eaa793
Eric Nerby <ericnerby@msn.com>
2021-04-28 15:43:29 -0400

    add cond example

---
+++ playground/day_of_week.exs
@@ -0,0 +1,14 @@
+defmodule Sample.Calendar do
+  def day_abbreviation(day) do
+    cond do
+      day == :Monday -> "M"
+      day == :Tuesday -> "Tu"
+      day == :Wednesday -> "W"
+      day == :Thursday -> "Th"
+      day == :Friday -> "F"
+      day == :Saturday -> "Sa"
+      day == :Sunday -> "Su"
+      true -> "Invalid day"
+    end
+  end
+end
