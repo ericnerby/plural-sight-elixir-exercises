878dd2b6f023e00931721ceead3511430b33e86f
Eric Nerby <ericnerby@msn.com>
2021-04-28 21:59:52 -0400

    recursion example

--- playground/sample_enum.exs
+++ playground/sample_enum.exs
@@ -1,15 +1,10 @@
 defmodule Sample.Enum do
-
   def first(list, val \\ nil)
   def first([head | _], _), do: head
   def first([], val), do: val 
 
-  def add(list, val \\ 0) do
-    trace(val)
-    [val | list]
-  end
-
-  defp trace(string) do
-    IO.puts("The value passed in was #{string}")
+  def map([], _), do: []
+  def map([hd | tl], f) do
+    [f.(hd) | map(tl, f)]
   end
 end
