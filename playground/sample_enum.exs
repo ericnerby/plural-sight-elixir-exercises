2644d2304912631418a3b8175a3c6fa2fd401fed
Eric Nerby <ericnerby@msn.com>
2021-04-28 22:08:41 -0400

    length function

--- playground/sample_enum.exs
+++ playground/sample_enum.exs
@@ -1,10 +1,15 @@
 defmodule Sample.Enum do
+  import Kernel, except: [length: 1]
+
   def first(list, val \\ nil)
   def first([head | _], _), do: head
   def first([], val), do: val 
 
   def map([], _), do: []
-  def map([hd | tl], f) do
-    [f.(hd) | map(tl, f)]
-  end
+  def map([hd | tl], f),
+    do: [f.(hd) | map(tl, f)]
+
+  def length([]), do: 0
+  def length([_ | tail]),
+    do: 1 + length(tail)
 end
