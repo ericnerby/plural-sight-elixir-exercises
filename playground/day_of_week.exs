9dafc086a7fb2309fb5d92203be9354c75fbae7c
Eric Nerby <ericnerby@msn.com>
2021-04-28 21:55:28 -0400

    describe_date function

--- playground/day_of_week.exs
+++ playground/day_of_week.exs
@@ -20,4 +20,29 @@ defmodule Sample.Calendar do
   def day_abbreviation2(:Saturday), do: "Sa"
   def day_abbreviation2(:Sunday), do: "Su"
   def day_abbreviation2(_), do: "Invalid Day"
+
+  def day_abbreviation3(day) do
+    case day do
+      :Monday -> "M"
+      :Tuesday -> "Tu"
+      :Wednesday -> "W"
+      :Thursday -> "Th"
+      :Friday -> "F"
+      :Saturday -> "Sa"
+      :Sunday -> "Su"
+      _ -> "Invalid Day"
+    end
+  end
+
+  def describe_date(date) do
+    case date do
+      {1, _, _} -> "Brand new month!"
+      {25, 12, _} -> "Merry Christmas"
+      {25, month, _} -> "Only #{12 - month} more months until Christmas"
+      {31, 10, 1517} -> "The reformation is starting."
+      {31, 10, _} -> "Happy Halloween"
+      {_, month, _} when month <= 12 -> "Just an average day"
+      {_, _, _} -> "Invalid month"
+    end
+  end
 end
