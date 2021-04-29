8f0cb0c2ac48d3bae8739b9cd6eb0c9d73391116
Eric Nerby <ericnerby@msn.com>
2021-04-29 14:05:32 -0400

    start implementing unit tests

--- pluralsight_tweet/test/pluralsight_tweet_test.exs
+++ pluralsight_tweet/test/pluralsight_tweet_test.exs
@@ -2,11 +2,4 @@ defmodule PluralsightTweetTest do
   use ExUnit.Case
   doctest PluralsightTweet
 
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
-
-  test "another test" do
-    assert 2 + 2 == 4
-  end
 end
