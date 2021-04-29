829ada88382406c5de119f5f0900bc72dceb984f
Eric Nerby <ericnerby@msn.com>
2021-04-28 22:12:30 -0400

    tail recursion

--- playground/sample_enum.exs
+++ playground/sample_enum.exs
@@ -12,4 +12,11 @@ defmodule Sample.Enum do
   def length([]), do: 0
   def length([_ | tail]),
     do: 1 + length(tail)
+  
+  def other_length([_ | tail]),
+    do: other_length(tail, 1)
+  def other_length([], len),
+    do: len
+  def other_length([_ | tail], len),
+    do: other_length(tail, len + 1)
 end
